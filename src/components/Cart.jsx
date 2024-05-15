400f560081f5661c27b22a490b8d784237472379
fmfahath <fmfahath.dev@gmail.com>
2024-05-15 23:42:31 +0530

    cart componet created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import './Cart.css'
 
 const Cart = () => {
     return (
-        <div>Cart</div>
+        <div className='add-cart-container'>
+            <div className="add-cart-view">
+                <div className="add-cart-image">
+                    <img src="" alt="" />
+                </div>
+                <div className="add-cart-detail">
+                    <p>Product Name</p>
+                    <p>Price</p>
+                </div>
+                <div className="add-cart-btn">
+                    <button>Remove</button>
+                </div>
+            </div>
+
+            <div className="add-cart-view">
+                <div className="add-cart-image">
+                    <img src="" alt="" />
+                </div>
+                <div className="add-cart-detail">
+                    <p>Product Name</p>
+                    <p>Price</p>
+                </div>
+                <div className="add-cart-btn">
+                    <button>Remove</button>
+                </div>
+            </div>
+
+            <div className="add-cart-view">
+                <div className="add-cart-image">
+                    <img src="" alt="" />
+                </div>
+                <div className="add-cart-detail">
+                    <p>Product Name</p>
+                    <p>Price</p>
+                </div>
+                <div className="add-cart-btn">
+                    <button>Remove</button>
+                </div>
+            </div>
+
+            <div className="total">
+                <p>Total: $000</p>
+            </div>
+        </div>
     )
 }
 
